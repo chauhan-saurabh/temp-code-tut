813d32bb77e02684bf9090036856f79409a6356d
Saurabh Chauhan <Saurabh.Chauhan@MANDSINDIA.NET>
2021-04-07 11:31:40 +0530

    initial commit

---
+++ 1-intro.js
@@ -0,0 +1,20 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("Small number");
+} else {
+  console.log("large number");
+}
+
+console.log(`hey it's my first node app`);
+
+const john = "john";
+const peter = "peter";
+
+const sayHi = (name) => {
+  console.log("the name is ", name);
+};
+
+sayHi("susan");
+sayHi(john);
+sayHi(peter);
